fb477c4208e6bbf27f988a90e31d6f385b1cec8e
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-06 06:06:55 +0000

    * Update to v1.1 * Wrap all functions in if(!function_exists()) check * Remove docs from top of plugin file (all that and more are in readme.txt) * Note compatibility with WP 2.9+, 3.0+ * Add PHPDoc documentation * Minor tweaks to code formatting (spacing) * Add package info to top of plugin file * Add Changelog, Template Tags, and Upgrade Notice sections to readme.txt * Update copyright date * Remove trailing whitespace
    git-svn-id: https://plugins.svn.wordpress.org/parentless-categories/trunk@285626 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- parentless-categories.php
+++ parentless-categories.php
@@ -1,73 +1,31 @@
 <?php
+/**
+ * @package Parentless_Categories
+ * @author Scott Reilly
+ * @version 1.1
+ */
 /*
 Plugin Name: Parentless Categories
-Version: 1.0
-Plugin URI: http://coffee2code.com/wp-plugins/parentless-categories
+Version: 1.1
+Plugin URI: http://coffee2code.com/wp-plugins/parentless-categories/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Like the_category(), list categories assigned to a post, but excluding assigned categories that have a child category also assigned to the post.
 
-    This plugin provides a template tag which acts a modified version of WordPress's built-in template tag, `the_category()`.
-	`the_category()` lists all categories directly assigned to the specified post.  `parentless_categories()` lists those
-	categories, except for categories that are parents to other assigned categories.
+Compatible with WordPress 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+.
 
-    For example, assume your category structure is hierarchical and looks like this:
-
-    Vegetables
-    |-- Leafy
-    |   |-- Broccoli
-    |   |-- Bok Choy
-    |   |-- Celery
-    |-- Fruiting
-    |   |-- Bell Pepper
-    |   |-- Cucumber
-    |   |-- Pumpkin
-    |-- Podded
-    |   |-- Chickpea
-    |   |-- Lentil
-    |   |-- Soybean
-
-    If you directly assigned the categories "Fruiting", "Cucumber", and "Pumpkin" to a post, `parentless_categories()` would return
-	a list that consists of: "Cucumber", and "Pumpkin".  Notice that since "Fruiting" was a parent to a directly assigned category,
-	it is not included in the list.
-
-    By default, categories are listed as an HTML list.  The first argment to the template tag allows you to define a custom separator,
-	e.g. to have a simple comma-separated list of categories: `<?php parentless_categories(','); ?>`.
-
-    As with categories listed via `the_category()`, categories that are listed are presented as links to the respective category's
-	archive page.
-
-    Example usage (based on preceeding example):
-
-    <?php parentless_categories(); ?>
-    Displays something like:
-        <ul><li><a href="http://yourblog.com/category/fruiting/cucumber">Cucumber</a></li>
-        <li><a href="http://yourblog.com/category/fruiting/pumpkin">Pumpkin</a></li></ul>
-
-    <?php parentless_categories(','); ?></ul>
-    Displays something like:
-        `<a href="http://yourblog.com/category/fruiting/cucumber">Cucumber</a>, <a href="http://yourblog.com/category/fruiting/pumpkin">Pumpkin</a>`
-
-Compatible with WordPress 2.5+, 2.6+, 2.7+, 2.8+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/parentless-categories.zip and unzip it into your 
-wp-content/plugins/ directory.
-2. (optional) Add filters for 'parentless_category' to filter parent categories from listing
-3. Use the function parentless_category() somewhere inside "the loop"
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/parentless-categories/
 
 */
 
 /*
-Copyright (c) 2008-2009 by Scott Reilly (aka coffee2code)
+Copyright (c) 2008-2010 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -78,24 +36,45 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-function parentless_categories($separator = '', $post_id = false) {
-	echo get_parentless_categories_list($separator, $post_id);	
+
+if ( !function_exists( 'parentless_categories' ) ) :
+/**
+ * Outputs the parentless categories.
+ *
+ * For use in the loop
+ *
+ * @param string $separator (optional) String to use as the separator
+ * @param int|false $post_id (optional) Post ID. If 'false', then the current post is assumed.  Default is 'false'.
+ * @return void (Text is echoed)
+*/
+function parentless_categories( $separator = '', $post_id = false ) {
+	echo get_parentless_categories_list( $separator, $post_id );
 }
+endif;
 
-function get_parentless_categories_list($separator = '', $post_id = false) {
+
+if ( !function_exists( 'get_parentless_categories_list' ) ) :
+/**
+ * Gets the list of parentless categories.
+ *
+ * @param string $separator (optional) String to use as the separator
+ * @param int|false $post_id (optional) Post ID. If 'false', then the current post is assumed.  Default is 'false'.
+ * @return string The HTML formatted list of parentless categories
+ */
+function get_parentless_categories_list( $separator = '', $post_id = false ) {
 	global $wp_rewrite;
-	$categories = get_parentless_categories($post_id);
-	if ( empty($categories) )
-		return apply_filters('parentless_categories', __('Uncategorized'), $separator);
+	$categories = get_parentless_categories( $post_id );
+	if ( empty( $categories ) )
+		return apply_filters( 'parentless_categories', __( 'Uncategorized' ), $separator );
 
-	$rel = ( is_object($wp_rewrite) && $wp_rewrite->using_permalinks() ) ? 'rel="category tag"' : 'rel="category"';
+	$rel = ( is_object( $wp_rewrite ) && $wp_rewrite->using_permalinks() ) ? 'rel="category tag"' : 'rel="category"';
 
 	$thelist = '';
 	if ( '' == $separator ) {
 		$thelist .= '<ul class="post-categories">';
 		foreach ( $categories as $category ) {
 			$thelist .= "\n\t<li>";
-			$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>' . $category->cat_name.'</a></li>';
+			$thelist .= '<a href="' . get_category_link( $category->term_id ) . '" title="' . sprintf( __( 'View all posts in %s' ), $category->name ) . '" ' . $rel . '>' . $category->cat_name.'</a></li>';
 		}
 		$thelist .= '</ul>';
 	} else {
@@ -103,31 +82,42 @@ function get_parentless_categories_list($separator = '', $post_id = false) {
 		foreach ( $categories as $category ) {
 			if ( 0 < $i )
 				$thelist .= $separator . ' ';
-			$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>' . $category->name.'</a>';
+			$thelist .= '<a href="' . get_category_link( $category->term_id ) . '" title="' . sprintf( __( 'View all posts in %s' ), $category->name ) . '" ' . $rel . '>' . $category->name.'</a>';
 			++$i;
 		}
 	}
-	return apply_filters('parentless_categories', $thelist, $separator);
+	return apply_filters( 'parentless_categories', $thelist, $separator );
 }
+endif;
 
-function get_parentless_categories( $id = false ) {
-	$categories = get_the_category($id);
-	if ( empty($categories) ) {
-#TODO
-#		return get_categories(array('hide_empty' => false, 'user_desc_for_title' => false, 'title_li' => '', 'child_of' => 0, 'depth' => 1));
-	}
+
+if ( !function_exists( 'get_parentless_categories' ) ) :
+/**
+ * Returns the list of parentless categories for the specified post. IF not supplied a
+ * post ID, then an empty array is returned.
+ *
+ * @param int|false $post_id (optional) Post ID. If 'false', then the current post is assumed.  Default is 'false'.
+ * @return array The array of parentless categories for the given category. If false, then assumes a top-level category.
+ */
+function get_parentless_categories( $post_id = false ) {
+	$categories = get_the_category( $post_id );
+	if ( empty( $categories ) )
+		return array();
 
 	$cats = array();
 	$parents = array();
 
 	// Go through all categories and get, then filter out, parents.
-	foreach ( $categories as $c ) { $parents[] = $c->parent; }
+	foreach ( $categories as $c )
+		$parents[] = $c->parent;
 	foreach ( $categories as $c ) {
-		if ( !in_array($c->term_id, $parents) ) { $cats[] = $c; }
+		if ( !in_array( $c->term_id, $parents ) )
+			$cats[] = $c;
 	}
 	
-	usort($cats, '_usort_terms_by_name');
+	usort( $cats, '_usort_terms_by_name' );
 	return $cats;
 }
+endif;
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,11 +1,11 @@
 === Parentless Categories ===
-Contributors: Scott Reilly
+Contributors: coffee2code
 Donate link: http://coffee2code.com/donate
 Tags: categories, category, list, the_category, coffee2code
 Requires at least: 2.5
-Tested up to: 2.8
-Stable tag: 1.0
-Version: 1.0
+Tested up to: 3.0.1
+Stable tag: 1.1
+Version: 1.1
 
 Like the_category(), list categories assigned to a post, but excluding assigned categories that have a child category also assigned to the post.
 
@@ -17,19 +17,33 @@ This plugin provides a template tag which acts a modified version of WordPress's
 
 For example, assume your category structure is hierarchical and looks like this:
 
+`
 Vegetables
+
 |-- Leafy
+
 |   |-- Broccoli
+
 |   |-- Bok Choy
+
 |   |-- Celery
+
 |-- Fruiting
+
 |   |-- Bell Pepper
+
 |   |-- Cucumber
+
 |   |-- Pumpkin
+
 |-- Podded
+
 |   |-- Chickpea
+
 |   |-- Lentil
+
 |   |-- Soybean
+`
 
 If you directly assigned the categories "Fruiting", "Cucumber", and "Pumpkin" to a post, `parentless_categories()` would return a list that consists of: "Cucumber", and "Pumpkin".  Notice that since "Fruiting" was a parent to a directly assigned category, it is not included in the list.
 
@@ -37,29 +51,83 @@ By default, categories are listed as an HTML list.  The first argment to the tem
 
 As with categories listed via `the_category()`, categories that are listed are presented as links to the respective category's archive page.
 
-Example usage (based on preceeding example):
+Example usage (based on preceding example):
 
-`<?php parentless_categories(); ?>`
-Displays something like:
-    `<ul><li><a href="http://yourblog.com/category/fruiting/cucumber">Cucumber</a></li>
-    <li><a href="http://yourblog.com/category/fruiting/pumpkin">Pumpkin</a></li></ul>`
+* `<?php parentless_categories(); ?>`
 
-`<?php parentless_categories(','); ?></ul>`
 Displays something like:
-    `<a href="http://yourblog.com/category/fruiting/cucumber">Cucumber</a>, <a href="http://yourblog.com/category/fruiting/pumpkin">Pumpkin</a>`
+
+`<ul><li><a href="http://yourblog.com/category/fruiting/cucumber">Cucumber</a></li>
+<li><a href="http://yourblog.com/category/fruiting/pumpkin">Pumpkin</a></li></ul>`
+
+* `<?php parentless_categories(','); ?></ul>`
+
+Displays something like:
+
+`<a href="http://yourblog.com/category/fruiting/cucumber">Cucumber</a>, <a href="http://yourblog.com/category/fruiting/pumpkin">Pumpkin</a>`
 
 
 == Installation ==
 
-1. Unzip `parentless-categories.zip` inside the `/wp-content/plugins/` directory for your site
+1. Unzip `parentless-categories.zip` inside the `/wp-content/plugins/` directory for your site (or install via the built-in WordPress plugin installer)
 1. Activate the plugin through the 'Plugins' admin menu in WordPress
 1. (optional) Add filters for 'parentless_categories' to filter parentless category listing
 1. Use the template tag `<?php parentless_categories(); ?>` somewhere inside "the loop"
 
+
 == Frequently Asked Questions ==
 
-= Why isn't an assigned category for the post showing up in the 'parentless_categories()' listing? =
+= Why isn't an assigned category for the post showing up in the `parentless_categories()` listing? =
 
 If an assigned category is the parent for one or more other assigned categories for the post, then the category parent is not included in the listing.
 
 
+== Template Tags ==
+
+The plugin provides three optional template tag for use in your theme templates.
+
+= Functions =
+
+* `<?php function parentless_categories( $separator = '', $post_id = false ) ?>`
+Outputs the parentless categories.
+
+* `<?php function get_parentless_categories_list( $separator = '', $post_id = false ) ?>`
+Gets the list of parentless categories.
+
+* `<?php function get_parentless_categories( $post_id = false ) ?>`
+Returns the list of parentless categories for the specified post.
+
+= Arguments =
+
+* `$separator`
+Optional argument. (string) String to use as the separator.
+
+* `$post_id`
+Optional argument. (int) Post ID.  If 'false', then the current post is assumed.  Default is 'false'.
+
+= Examples =
+
+* (See Description section)
+
+
+== Changelog ==
+
+= 1.1 =
+* Wrap all functions in if(!function_exists()) check
+* Remove docs from top of plugin file (all that and more are in readme.txt)
+* Note compatibility with WP 2.9+, 3.0+
+* Add PHPDoc documentation
+* Minor tweaks to code formatting (spacing)
+* Add package info to top of plugin file
+* Add Changelog, Template Tags, and Upgrade Notice sections to readme.txt
+* Update copyright date
+* Remove trailing whitespace
+
+= 1.0 =
+* Initial release
+
+
+== Upgrade Notice ==
+
+= 1.1 =
+Minor update. Highlights: miscellaneous non-functionality tweaks; verified WP 3.0 compatibility.
